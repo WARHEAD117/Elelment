b76613fdf12a9bf1556b6e93d63b9f8ed402d324
Ponta Nekoi <pontanekoi@gmail.com>
2017-07-11 17:58:39 +0900

    LevelEdition
    Level edition & Unity update to ver. 5.6.1f1

---
+++ Prototype/Element/Assets/Script/Elements/Thunder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thunder : MonoBehaviour {
+
+	public GameObject ThunderSpawn;
+
+	float time = 0f;
+
+	void Awake(){
+		ThunderSpawn.SetActive (false);
+	}
+
+	void Update()
+	{
+		time += Time.deltaTime;
+		if(time >= 5)
+		{
+			ThunderSpawn.SetActive (true);
+			if (time >= 8) {
+				ThunderSpawn.SetActive (false);
+				time = 0;
+			}
+		}
+	}
+}
